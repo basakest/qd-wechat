bcae166f6b5ee51ebe9d729172839ad9038c7479
basakest <ab1652759879@gmail.com>
2021-03-18 19:24:20 +0800

    安装对应的两个扩展dcat/laravel-admin和overtrue/laravel-wechat

---
+++ resources/lang/en/user.php
@@ -0,0 +1,15 @@
+<?php 
+return [
+    'labels' => [
+        'User' => 'User',
+    ],
+    'fields' => [
+        'email' => '邮箱地址',
+        'email_verified_at' => '邮箱认证时间',
+        'name' => '用户名',
+        'password' => '密码',
+        'remember_token' => 'remember token',
+    ],
+    'options' => [
+    ],
+];
